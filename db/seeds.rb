826334545214b3b623010cd28644af96234a9eef
Afzal Rahman <afzalrahman123@gmail.com>
2021-07-17 14:36:24 +0530

    Add users resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Afzal", password: "afzal123")
+User.create(username: "Majid", password: "majid123")
+User.create(username: "Arjun", password: "arjun123")
+User.create(username: "Jabir", password: "jabir123")
+User.create(username: "Muhammed", password: "muhammed123")
